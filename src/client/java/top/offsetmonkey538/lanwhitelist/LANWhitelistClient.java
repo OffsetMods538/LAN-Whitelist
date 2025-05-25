cf5abe4e4307c6399be4acde124b67a3c7594408
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-05-25 09:48:20 +0300

    Initial Commit

---
+++ src/client/java/top/offsetmonkey538/lanwhitelist/LANWhitelistClient.java
@@ -0,0 +1,11 @@
+package top.offsetmonkey538.lanwhitelist;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LANWhitelistClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// Do stuff
+	}
+}
