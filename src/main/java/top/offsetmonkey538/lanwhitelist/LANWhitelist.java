9e7e8584da0037d38f8ef7251335e9a3aed2b59a
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-05-25 10:30:20 +0300

    Ok cool vanilla player manager stuff can be used

--- src/main/java/top/offsetmonkey538/lanwhitelist/LANWhitelist.java
+++ src/main/java/top/offsetmonkey538/lanwhitelist/LANWhitelist.java
@@ -1,9 +1,13 @@
 package top.offsetmonkey538.lanwhitelist;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.WhitelistEntry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static net.minecraft.server.command.CommandManager.literal;
 
 public class LANWhitelist implements ModInitializer {
 	public static final String MOD_ID = "lan-whitelist";
@@ -11,6 +15,16 @@ public class LANWhitelist implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		CommandRegistrationCallback.EVENT.register((commandDispatcher, commandRegistryAccess, registrationEnvironment) -> {
+			commandDispatcher.register(
+					literal("whitelist")
+							.executes(context -> {
+								context.getSource().getServer().getPlayerManager().setWhitelistEnabled(true);
+								context.getSource().getServer().getPlayerManager().getWhitelist().add(new WhitelistEntry(context.getSource().getPlayerOrThrow().getGameProfile()));
+								return 1;
+							})
+			);
+		});
 	}
 
 	public static Identifier id(String path) {
