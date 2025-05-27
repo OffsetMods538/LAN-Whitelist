87dcf6cc859fe3dfa0e48311e16c120324af0ad6
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-05-27 14:48:16 +0300

    Literally more black magic (add support for 1.21.5)

--- src/client/java/top/offsetmonkey538/lanwhitelist/persistent/WhitelistPersistentState.java
+++ src/client/java/top/offsetmonkey538/lanwhitelist/persistent/WhitelistPersistentState.java
@@ -1,10 +1,13 @@
 package top.offsetmonkey538.lanwhitelist.persistent;
 
 import com.mojang.datafixers.util.Function4;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
 import net.minecraft.datafixer.DataFixTypes;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
@@ -24,15 +27,12 @@ import static top.offsetmonkey538.lanwhitelist.LANWhitelist.MOD_ID;
 
 public class WhitelistPersistentState extends PersistentState {
     private static final String ENABLED_KEY = "enabled";
+    private static final Codec<WhitelistPersistentState> CODEC = Codec.BOOL.xmap(WhitelistPersistentState::new, state -> state.enabled);
 
     public Boolean enabled = false;
 
-    // At some point, Minecraft started using the registry lookup stuff, but it's not available in 1.20, so I have implementations both with and without the lookup
-    private WhitelistPersistentState(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
-        this(nbt);
-    }
-    private WhitelistPersistentState(NbtCompound nbt) {
-        this.enabled = nbt.getBoolean(ENABLED_KEY);
+    private WhitelistPersistentState(boolean enabled) {
+        this.enabled = enabled;
     }
 
     private WhitelistPersistentState() {
@@ -54,11 +54,11 @@ public class WhitelistPersistentState extends PersistentState {
         final ServerWorld world = server.getWorld(World.OVERWORLD);
         if (world == null) throw new IllegalStateException();
 
-        return PersistentStateHandler.getOrCreate.apply(world.getPersistentStateManager(), WhitelistPersistentState::new, WhitelistPersistentState::new, MOD_ID);
+        return VersionsHandler.getOrCreate.getOrCreate(world.getPersistentStateManager(), WhitelistPersistentState.CODEC, WhitelistPersistentState::new, MOD_ID);
     }
 
     // Version at the end of variables signifies the last supported version
-    private static class PersistentStateHandler {
+    private static class VersionsHandler {
         private static final MappingResolver resolver = FabricLoader.getInstance().getMappingResolver();
 
         private static final String getOrCreateV1d20d1 = resolver.mapMethodName(
@@ -88,16 +88,35 @@ public class WhitelistPersistentState extends PersistentState {
                 )
         );
 
+        private static final String typeV1d21d5 = resolver.mapClassName(
+                "intermediary",
+                "net.minecraft.class_10741" // PersistentState.Type
+        );
+        private static final String getOrCreateV1d21d5 = resolver.mapMethodName(
+                "intermediary",
+                "net.minecraft.class_26", // PersistentStateManager
+                "method_17924", // getOrCreate
+                org.objectweb.asm.Type.getMethodDescriptor(
+                        org.objectweb.asm.Type.getObjectType("net/minecraft/class_18"), // PersistentState
+                        org.objectweb.asm.Type.getObjectType("net.minecraft.class_10741") // PersistentStateType
+                )
+        );
 
-        private static final Function4<PersistentStateManager, Function<NbtCompound, WhitelistPersistentState>, Supplier<WhitelistPersistentState>, String, WhitelistPersistentState> getOrCreate;
+
+        private static final GetOrCreateMethod getOrCreate;
 
         static {
-            Function4<PersistentStateManager, Function<NbtCompound, WhitelistPersistentState>, Supplier<WhitelistPersistentState>, String, WhitelistPersistentState> getOrCreate1;
+            GetOrCreateMethod result;
             try {
                 final Method method = PersistentStateManager.class.getDeclaredMethod(getOrCreateV1d20d1, Function.class, Supplier.class, String.class);
-                getOrCreate1 = (manager, readFunction, supplier, id) -> {
+                result = (manager, codec, emptySupplier, id) -> {
                     try {
-                        return (WhitelistPersistentState) method.invoke(manager, readFunction, supplier, id);
+                        return (WhitelistPersistentState) method.invoke(
+                                manager,
+                                (Function<NbtCompound, WhitelistPersistentState>) nbt -> CODEC.parse(NbtOps.INSTANCE, nbt).resultOrPartial(LANWhitelist.LOGGER::error).orElse(new WhitelistPersistentState()),
+                                emptySupplier,
+                                id
+                        );
                     } catch (IllegalAccessException | InvocationTargetException e) {
                         throw new RuntimeException(e);
                     }
@@ -115,18 +134,54 @@ public class WhitelistPersistentState extends PersistentState {
 
                     final Method method = PersistentStateManager.class.getDeclaredMethod(getOrCreateV1d21d4, typeClass, String.class);
 
-                    getOrCreate1 = (manager, readFunction, supplier, id) -> {
+                    result = (manager, codec, emptySupplier, id) -> {
                         try {
-                            return (WhitelistPersistentState) method.invoke(manager, typeConstructor.newInstance(supplier, (BiFunction<NbtCompound, ?, WhitelistPersistentState>) (nbt, lookup) -> readFunction.apply(nbt), null), id);
+                            return (WhitelistPersistentState) method.invoke(
+                                    manager,
+                                    typeConstructor.newInstance(emptySupplier, (BiFunction<NbtCompound, ?, WhitelistPersistentState>) (nbt, lookup) -> CODEC.parse(NbtOps.INSTANCE, nbt).resultOrPartial(LANWhitelist.LOGGER::error).orElse(new WhitelistPersistentState()), null),
+                                    id
+                            );
                         } catch (IllegalAccessException | InvocationTargetException | InstantiationException ex) {
                             throw new RuntimeException(ex);
                         }
                     };
                 } catch (ClassNotFoundException | NoSuchMethodException e2) {
-                    throw new RuntimeException("1.21.4 also didn't match!", e2);
+                    LANWhitelist.LOGGER.info("1.21.4 getOrCreate doesn't exist, trying 1.21.5", e2);
+
+                    try {
+                        final Class<?> typeClass = Class.forName(typeV1d21d5);
+                        final Constructor<?> typeConstructor = typeClass.getConstructor(
+                                String.class,
+                                Supplier.class,
+                                Codec.class,
+                                DataFixTypes.class
+                        );
+
+                        final Method method = PersistentStateManager.class.getDeclaredMethod(getOrCreateV1d21d5, typeClass);
+
+                        result = (manager, codec, emptySupplier, id) -> {
+                            try {
+                                return (WhitelistPersistentState) method.invoke(manager, typeConstructor.newInstance(
+                                        id,
+                                        emptySupplier,
+                                        codec,
+                                        null
+                                ));
+                            } catch (InvocationTargetException | IllegalAccessException | InstantiationException ex) {
+                                throw new RuntimeException(ex);
+                            }
+                        };
+                    } catch (ClassNotFoundException | NoSuchMethodException e3) {
+                        throw new RuntimeException("1.21.5 also didn't match!", e3);
+                    }
                 }
             }
-            getOrCreate = getOrCreate1;
+            getOrCreate = result;
         }
+    }
+
+    @FunctionalInterface
+    private interface GetOrCreateMethod {
+        WhitelistPersistentState getOrCreate(PersistentStateManager manager, Codec<WhitelistPersistentState> codec, Supplier<WhitelistPersistentState> emptySupplier, String id);
     }
 }
